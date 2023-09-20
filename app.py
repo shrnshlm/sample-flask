a4ea1e7482885583d4debdb69a77c3445b66eb7f
Sharon Shalem <shrnshlm@gmail.com>
2023-09-20 14:03:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/')  
+def upload():  
+    return render_template("index.html")  
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(f.filename)  
+        return render_template("success.html", name = f.filename)  
+
+if __name__ == '__main__':  
+    app.run(host= '0.0.0.0', debug = True)  
+    

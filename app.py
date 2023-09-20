31a546008a4d448f101b1998f1018f71ff9d7502
Sharon Shalem <shrnshlm@gmail.com>
2023-09-20 16:28:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import *  
+import cv2
 import base64
+import numpy as np
+from rembg import remove
+
 
 app = Flask(__name__)
 
@@ -13,7 +17,15 @@ def success():
         f = request.files['file']
         if f.filename != '':
             image_string = base64.b64encode(f.read())
-            return render_template("success.html", content = image_string.decode('utf-8'))
+
+            decodedInput = base64.b64decode(image_string)
+            inputNpData = np.frombuffer(decodedInput, np.uint8)
+            input = cv2.imdecode(inputNpData, cv2.IMREAD_UNCHANGED)
+            output = remove(input)
+            # output = input
+            retval, buffer = cv2.imencode('.png', output)
+            outputImage = base64.b64encode(buffer)
+            return render_template("success.html", content = outputImage.decode('utf-8'))
 
 if __name__ == '__main__':  
     app.run(host= '0.0.0.0', debug = True)  

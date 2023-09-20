84e99078199f7599a110b735c9d62ac3b53e6184
Sharon Shalem <shrnshlm@gmail.com>
2023-09-20 15:56:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import *  
+import base64
+
 app = Flask(__name__)
 
 @app.route('/')  
@@ -8,9 +10,10 @@ def upload():
 @app.route('/success', methods = ['POST'])  
 def success():  
     if request.method == 'POST':  
-        f = request.files['file']  
-        f.save(f.filename)  
-        return render_template("success.html", name = f.filename)  
+        f = request.files['file']
+        if f.filename != '':
+            image_string = base64.b64encode(f.read())
+            return render_template("success.html", content = image_string.decode('utf-8'))
 
 if __name__ == '__main__':  
     app.run(host= '0.0.0.0', debug = True)  
